4c3d32c0c79ff8a8d6c4bb312841f0decffa7572
Oleksandr Davydovskyi <oleksandr.davydovskyi@plus4u.net>
2024-09-14 16:51:05 +0200

    Refactor pizza block and header styles

--- frontend/src/components/PizzaBlock/index.jsx
+++ frontend/src/components/PizzaBlock/index.jsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 const PizzaBlock = ({
   id,
   imageUrl,
-  name,
+  title,
   types,
   sizes,
   price,
@@ -15,9 +15,10 @@ const PizzaBlock = ({
   const [activeSize, setActiveSize] = useState(0);
 
   return (
-    <div className="pizza-block">
+    <div className="pizza-block-wrapper">
+      <div className="pizza-block">
       <img className="pizza-block__image" src={imageUrl} alt="Pizza" />
-      <h4 className="pizza-block__title">{name}</h4>
+      <h4 className="pizza-block__title">{title}</h4>
       <div className="pizza-block__selector">
         <ul>
           {types.map((type, index) => (
@@ -61,6 +62,7 @@ const PizzaBlock = ({
           <i>{count}</i>
         </div>
       </div>
+    </div>
     </div>
   );
 };
